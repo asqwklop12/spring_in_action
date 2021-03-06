9589ebfed8a3ef97983eee5569374bccdd2b94be
asqwklop12 <asqwklop12@gmail.com>
2021-03-06 15:27:03 +0900

    2장

---
+++ src/main/java/klom/spring/spring_in_action/web/WebConfig.java
@@ -0,0 +1,14 @@
+package klom.spring.spring_in_action.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("home");
+  }
+}
--- src/test/java/klom/spring/spring_in_action/HomeControllerTest.java
+++ src/test/java/klom/spring/spring_in_action/HomeControllerTest.java
@@ -1,9 +1,7 @@
 package klom.spring.spring_in_action;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,10 +9,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
   @Autowired
@@ -23,9 +19,9 @@ class HomeControllerTest {
   @Test
   public void testHomePage() throws Exception {
     mockMvc.perform(get("/"))
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(view().name("home"))
-            .andExpect(content().string(containsString("Welcome to...")));
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("home"))
+        .andExpect(content().string(containsString("Welcome to...")));
   }
 }
