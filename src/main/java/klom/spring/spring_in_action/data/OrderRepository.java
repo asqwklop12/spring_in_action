a152a4ac7f8109c341e4d1f1bdb18b079cccdd80
asqwklop12 <asqwklop12@gmail.com>
2021-03-14 00:08:41 +0900

    5장

--- src/main/java/klom/spring/spring_in_action/data/OrderRepository.java
+++ src/main/java/klom/spring/spring_in_action/data/OrderRepository.java
@@ -1,8 +1,13 @@
 package klom.spring.spring_in_action.data;
 
+import java.util.List;
 import klom.spring.spring_in_action.Ingredient;
+import klom.spring.spring_in_action.Order;
+import klom.spring.spring_in_action.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Ingredient, String> {
 
+  List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
--- src/main/java/klom/spring/spring_in_action/web/OrderController.java
+++ src/main/java/klom/spring/spring_in_action/web/OrderController.java
@@ -3,9 +3,13 @@ package klom.spring.spring_in_action.web;
 import javax.validation.Valid;
 import klom.spring.spring_in_action.Order;
 import klom.spring.spring_in_action.User;
+import klom.spring.spring_in_action.data.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,6 +20,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
+
+  private final OrderRepository orderRepo;
+  //  private int pageSize = 20;
+//
+//  public void setPageSize(int pageSize) {
+//    this.pageSize = pageSize;
+//  }
+  private OrderProps props;
+
+  public OrderController(OrderRepository orderRepo,
+      OrderProps props) {
+    this.orderRepo = orderRepo;
+    this.props = props;
+  }
 
   @GetMapping("/current")
   public String orderForm(@AuthenticationPrincipal User user,
@@ -47,4 +65,14 @@ public class OrderController {
     log.info("Order submitted: " + order);
     return "redirect:/";
   }
+
+  @GetMapping
+  public String orderForUser(
+      @AuthenticationPrincipal User user, Model model
+  ) {
+    Pageable pageable = PageRequest.of(0, props.getPageSize());
+    model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+    return "orderList";
+  }
+
 }
